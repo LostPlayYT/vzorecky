c5f4c128afca543c02ccfc666cc1479893f8476f
Ladislav Hertl <Lostplayyt@seznam.cz>
2024-01-19 12:29:25 +0100

    První hodina

---
+++ obsah.py
@@ -0,0 +1,11 @@
+#Uživatele přivítáme v aplikaci
+print ("Vítejte v aplikaci pro výpočet obsahu kruhu")
+#Deklarace proměných a přetypování na str
+a = str(input("Zadejte poloměr: "))
+#Kontrolujeme záporná čísla
+if a>0:
+    print("Výsledek je: ")
+    print(3.14*a*a)
+#Pokud nebude platit první 
+else:
+    print ("Chyba")
